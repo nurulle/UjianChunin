431e50b0b5cf9f825e8d8c8a0c4a9b1e0cad1a7d
nurull.e <chotimahnurul23@gmail.com>
2021-02-13 14:03:26 +0700

    up2

--- Soal Ke -2/3/3.js
+++ Soal Ke -2/3/3.js
@@ -1,21 +1,3 @@
-
-// function piramidKebalik() {
-//   let string = '';
-//   for ( kolom = 4; kolom >= 1; kolom--) {
-//     for(baris = 4; baris > kolom; baris--){
-//       string += ' '
-//     }
-//     for(bawah = 1; bawah < (kolom * 2-1); bawah++){
-//       string += bawah;
-//     }
-//     string += '\n'
-//   }
-
-//   console.log(string)
-// } 
-// piramidKebalik();
-
-
 function piramidKebalik() {
   let string = '';
   for(i = 1; i <= 5; i++){
@@ -28,16 +10,6 @@ function piramidKebalik() {
     }
     string += '\n'
   }
-  // for ( kolom = 4; kolom >= 1; kolom--) {
-  //   for(baris = 4; baris > kolom; baris--){
-  //     string += ' '
-  //   }
-  //   for(bawah = 1; bawah < (kolom * 2-1); bawah++){
-  //     string += bawah;
-  //   }
-  //   string += '\n'
-  // }
-
   console.log(string)
 } 
 piramidKebalik();
